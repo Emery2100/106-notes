9cab074bcaaabea2735d2adc833e11bf51796867
Emery2100 <Emery2100@yahoo.com>
2025-07-17 18:37:19 -0700

    assigment-2

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,13 @@
-
-function hello(){
-    console.log("Hello there!");
+function saveTask(){
+    console.log("Hello im the saveButton")
 };
-
-
 
 function init(){
     console.log("Hello im the init");
-    hello();
+    $("#btnSave").click(saveTask);
 };
 
 
 
 window.onload = init; // it waits until the css and the html resolve to run the logic
+
